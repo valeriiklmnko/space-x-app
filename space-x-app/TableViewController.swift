bc092ffa4018a0aa38f250110e7f70c9c79a49a3
Valerii Klymenko <valerii.klymenko@applifting.cz>
2020-02-24 14:31:19 +0100

    Create SpaceX test app project. Create UITableViewController and add tableView methods. Create SpaceShipClass. Setup Main storyboard.

---
+++ space-x-app/TableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  space-x-app
+//
+//  Created by Valerii Klymenko on 24/02/2020.
+//  Copyright © 2020 Valerii Klymenko. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    var spaceShips: [SpaceShip] = [
+        SpaceShip(
+            rocketName: "Falcon 1",
+            launchDate: "2006-03-24T22:30:00.000Z",
+            launchYear: "2006",
+            missionName: "FalconSat",
+            launchSite: "Kwajalein Atoll Omelek Island"
+        ),
+        SpaceShip(
+            rocketName: "Falcon 1",
+            launchDate: "2007-03-21T01:10:00.000Z",
+            launchYear: "2007",
+            missionName: "DemoSat",
+            launchSite: "Kwajalein Atoll Omelek Island"
+        ),
+        SpaceShip(
+            rocketName: "Falcon 1",
+            launchDate: "2008-08-02T03:34:00.000Z",
+            launchYear: "2008",
+            missionName: "Trailblazer",
+            launchSite: "Kwajalein Atoll Omelek Island"
+        ),
+        SpaceShip(
+            rocketName: "Falcon 1",
+            launchDate: "2008-09-28T23:15:00.000Z",
+            launchYear: "2008",
+            missionName: "RatSat",
+            launchSite: "Kwajalein Atoll Omelek Island"
+        )
+    ]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spaceShips.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SpaceShipCell", for: indexPath)
+        
+        if let label = cell.viewWithTag(1) as? UILabel {
+            switch indexPath.row {
+            case 0:
+                label.text = "Mission: \(spaceShips[0].missionName), Year: \(spaceShips[0].launchYear)"
+            case 1:
+                label.text = "Mission: \(spaceShips[1].missionName), Year: \(spaceShips[1].launchYear)"
+            case 2:
+                label.text = "Mission: \(spaceShips[2].missionName), Year: \(spaceShips[2].launchYear)"
+            case 3:
+                label.text = "Mission: \(spaceShips[3].missionName), Year: \(spaceShips[3].launchYear)"
+            default:
+                label.text = "Correct Space Ship not found"
+            }
+        }
+        
+        return cell
+    }
+}
