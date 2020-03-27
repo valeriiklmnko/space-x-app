cf7f693a735a6338be65070b61c2e02cb45af66a
Valerii Klymenko <valerii.klymenko@applifting.cz>
2020-03-27 10:24:31 +0100

    Change displaying of Modal from Storyboard way to swift code way.

--- space-x-app/View Controllers/RocketsViewController.swift
+++ space-x-app/View Controllers/RocketsViewController.swift
@@ -13,4 +13,12 @@ class RocketsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func showModal(_ sender: Any) {
+        let modalStoryboard = UIStoryboard(name: "Modal", bundle: nil)
+        if let modalVC = modalStoryboard.instantiateViewController(identifier: "Modal") as? ModalViewController {
+            let navController = UINavigationController(rootViewController: modalVC)
+            self.navigationController?.present(navController, animated: true, completion: nil)
+        }
+    }
 }
