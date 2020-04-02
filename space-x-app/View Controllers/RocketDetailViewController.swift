03c0ff15446fec2b6364ebf10cd237bcbf550969
Valerii Klymenko <valerii.klymenko@applifting.cz>
2020-04-02 17:14:37 +0200

    Add image to Rocket Detail screen. Add animation to Rocket Detail screen.

--- space-x-app/View Controllers/RocketDetailViewController.swift
+++ space-x-app/View Controllers/RocketDetailViewController.swift
@@ -11,16 +11,76 @@ import UIKit
 class RocketDetailViewController: UIViewController {
     
     @IBOutlet weak var rocketName: UILabel!
+    @IBOutlet weak var rocketImage: UIImageView!
+    @IBOutlet weak var rocketNameCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var rocketImageCenterConstraint: NSLayoutConstraint!
     
     var selectedRocket: Rocket?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showRocket()
+        showTitle()
+        animateRocket()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureRocketDetailView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        rocketImageCenterConstraint.constant -= view.bounds.width
+        rocketNameCenterConstraint.constant -= view.bounds.width
+    }
+    
     func configureRocketDetailView() {
         self.title = selectedRocket?.rocketName
         self.rocketName.text = selectedRocket?.rocketName
+    }
+    
+    func showRocket() {
+        rocketImageCenterConstraint.constant = 0
+        UIView.animate(
+             withDuration: 0.5,
+             delay: 0,
+             options: [.curveEaseInOut],
+             animations: { [weak self] in
+                 self?.view.layoutIfNeeded()
+             },
+             completion: nil
+         )
+    }
+    
+    func animateRocket() {
+        let options: UIView.AnimationOptions = [
+            .curveEaseInOut,
+            .repeat,
+            .autoreverse
+        ]
+        UIView.animate(
+            withDuration: 3.0,
+            delay: 0.3,
+            options: options,
+            animations: { [weak self] in
+                self?.rocketImage.frame.size.height *= 1.18
+                self?.rocketImage.frame.size.width *= 1.18
+            },
+            completion: nil
+        )
+    }
+    
+    func showTitle() {
+        rocketNameCenterConstraint.constant = 0
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0.3,
+            options: [.curveEaseInOut],
+            animations: { [weak self] in
+                self?.view.layoutIfNeeded()
+            },
+            completion: nil
+        )
     }
 }
